a8cca3d78ec9d4c46422fcbd135f838bdd214932
paulseo1 <miyus1004@gmail.com>
2016-02-28 22:55:41 +0900

    LearningNode ch1

---
+++ LearningNode/ch1/helloworld.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	fs.readFile('helloworld.js', 'utf8', function(err, data) {
+		res.writeHead(200, {'Content=Type': 'text/plain'});
+
+		if (err)
+			res.write('Could not find or open file for reading\n');
+		else
+			res.write(data);
+		res.end();
+	});
+}).listen(10015, function() { console.log('bound to port 10015');});
+
+console.log('Server running on 10015/');
